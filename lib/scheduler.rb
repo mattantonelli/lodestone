1673379abab11be5aa6d3d696a929573ba12e72c
Matt Antonelli <mattr.antonelli@gmail.com>
2017-11-27 17:49:51 -0500

    Added lockfile for scheduler
    * Keeping the tmp directory to store the lockfile
    * Resolves #1

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -2,7 +2,7 @@ module Scheduler
   extend self
 
   def run(logger)
-    scheduler = Rufus::Scheduler.new
+    scheduler = Rufus::Scheduler.new(lockfile: 'tmp/.rufus-scheduler.lock')
 
     scheduler.cron('5,35 * * * *') do
       Webhooks.execute_all(logger)
