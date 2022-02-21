4db248a04b374c2cad10781142cb38da6c0026f5
Matt Antonelli <contact@raelys.com>
2022-02-21 12:49:44 +0000

    Force SSL, set correct cronjob schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,7 @@
 env :PATH, ENV['PATH']
 set :output, 'log/whenever.log'
 
-# TODO: Switch back to the %5 schedule when we are ready to go live
-# every '5,15,25,35,45,55 * * * *' do
-every '0,10,20,30,40,50 * * * *' do
+every '5,15,25,35,45,55 * * * *' do
   %w(na eu de fr jp).each do |locale|
     rake "news:deliver[#{locale}]"
   end
