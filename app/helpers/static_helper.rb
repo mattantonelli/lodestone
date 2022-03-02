3334680c7bff5a8c9eb7e3c13ab941b73c7ddf9a
Matt Antonelli <contact@raelys.com>
2022-03-02 02:32:36 +0000

    Add Oceania to Europe region selection

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,6 +1,6 @@
 module StaticHelper
   def locale_options(selected)
-    options_for_select([['North America', 'na'], ['Europe', 'eu'], ['France', 'fr'], ['Deutschland', 'de'], ['日本', 'jp']],
+    options_for_select([['North America', 'na'], ['Europe / Oceania', 'eu'], ['France', 'fr'], ['Deutschland', 'de'], ['日本', 'jp']],
                        selected)
   end
 end
